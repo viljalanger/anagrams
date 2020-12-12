2d030df25c3464fd24d0ad144c4c7ff62cb807a1
Yiyi Chen <chen.yiyi@hotmail.it>
2020-12-12 19:32:48 +0100

    Fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const { resolve } = require('path');
+const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	mode: 'none',
 	target: 'node',
 	entry: {
-		app: resolve(__dirname, 'src/main.ts'),
+		app: path.resolve(__dirname, 'src/main.ts'),
 	},
 	output: {
-		path: resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'dist'),
 		filename: 'main.js',
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new CopyWebpackPlugin({
-			patterns: [{ from: resolve(__dirname, 'assets/**/*.*'), to: './assets' }],
+			patterns: [{ from: path.join(__dirname, 'assets/**/*'), to: './' }],
 		}),
 	],
 	module: {
@@ -24,14 +24,13 @@ module.exports = {
 			{
 				test: /\.tsx?$/,
 				use: 'ts-loader',
-				exclude: [/node_modules/, /\.spec.tsx?$/],
+				exclude: /node_modules/,
 			},
 		],
 	},
 	resolve: {
 		plugins: [new TsconfigPathsPlugin()],
-		modules: [resolve(__dirname, 'node_modules'), 'node_modules'],
-		extensions: ['.ts', '.js'],
+		extensions: ['.tsx', '.ts', '.js'],
 		fallback: { constants: false },
 	},
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,6 @@
 const { merge } = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
 	mode: 'production',
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin({
-				terserOptions: {
-					output: { comments: false },
-				},
-				extractComments: false,
-			}),
-		],
-	},
 });
